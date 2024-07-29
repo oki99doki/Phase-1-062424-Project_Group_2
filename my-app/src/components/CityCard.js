6422be3b9a71c1c4a56e8d34d6d9540cf73422b9
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-07-29 16:20:35 -0400

    continued working on City List

--- my-app/src/components/CityCard.js
+++ my-app/src/components/CityCard.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 
 function CityCard({destination}) {
+const [favorite, setFavorite] = useState(true)
 
+
+const toggleFavorite = () => {
+    setFavorite(prev => !prev)
+}
+
+const handleSubmit = ((e) => {
+    e.preventDefault()
+    fetch(`http://localhost:4000/places/${id}`), {
+    method: 'Patch',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({favorite: !favorite})
+    }
+    .then(res => {
+        if(res.ok){
+            return res.json()
+        } else {
+            throw Error('patch failed')
+        }
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => console.error('couldnt reach server'))
+})
 
 
     return (
@@ -10,6 +36,11 @@ function CityCard({destination}) {
         <img src={destination.image}/>
         <h3>Country Name: {destination["country-name"]}</h3>
         <h4>City Name: {destination["city-name"]}</h4>
+        {favorite ? (
+        <button className="favorite" onClick={toggleFavorite}>favorite</button>
+      ) : (
+        <button onClick={toggleFavorite}>unfavorite</button>
+      )}
         </li>
     )
 };
