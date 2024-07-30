df32e1c3e400ec2fa2b05853d0cf59fed785189f
Stefan Klinkert <s.klinkert@gmail.com>
2024-07-30 18:43:50 +0200

    add search feature

--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -3,50 +3,64 @@ import Header from "./Header";
 import FavoriteForm from "./FavoriteForm";
 import NewDestinationForm from "./NewDestinationForm";
 import CityList from "./CityList";
+import Search from "./Search";
 
 function HomePage() {
+  const [destinations, setDestination] = useState([]);
+  //const [searchDestination, setSearch] = useState("")
+  const [search, setSearch] = useState("");
 
-const [destinations, setDestination] = useState([])
-const [searchPlaces, setSearchplaces] = useState("")
-
-useEffect(() => {
-    fetch('http://localhost:4000/places')
-    .then(res => res.json())
-    .then(data => setDestination(data))
-  }, [])
-
+  useEffect(() => {
+    fetch("http://localhost:4000/places")
+      .then((res) => res.json())
+      .then((data) => setDestination(data));
+  }, []);
 
   // const addPlaces = (newPlaces) => {
   //   setSearchplaces([...destinations, newPlaces])
   // };
 
   const addDestination = (newDestination) => {
-    setDestination([...destinations, newDestination])
-  }
+    setDestination([...destinations, newDestination]);
+  };
 
+  const updateSearch = (newSearch) => setSearch(newSearch);
 
+  
+  
+  const filteredDestinations = destinations.filter((curDestination) => {
 
+    console.log(curDestination)
+    
+    return (
+      curDestination["city-name"].toLowerCase().includes(search.toLowerCase()) ||
+      curDestination["city-name"].toLowerCase().includes(search.toLowerCase()) ||
+      curDestination["country-name"].toLowerCase().includes(search.toLowerCase()) ||
+      curDestination["country-name"].toLowerCase().includes(search.toLowerCase())
+    );
+
+  });
+  
 
 return (
-  <>
-  <header>
-  <Header />
-  </header>
-  
-  <h1> HomePage </h1>
-
   <div>
-  <FavoriteForm /> 
-  <NewDestinationForm addDestination={addDestination} />
+    <header>
+      <Header />
+    </header>
+
+    <div>
+    <h1> HomePage </h1>
+      <Search search={search} updateSearch={updateSearch} />
+      <FavoriteForm />
+      <NewDestinationForm addDestination={addDestination} />
+
+      {destinations.map((destination) => (
+        <CityList key={destination.id} destination={destination} />
+      ))}
+
+    </div>
   </div>
-
-  {
-  destinations.map(destination => <CityList key={destination.id} destination={destination} />)
-  }
-  
-  </>
-
-)
+);
 }
 
 export default HomePage;
---
+++ my-app/src/components/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Search({ search, updateSearch }) {
+  return (
+    <div className="searchbar">
+      <label htmlFor="search">Search Destinations:</label>
+      <input
+        value={search}
+        type="text"
+        id="search"
+        placeholder="Type a name to search..."
+        onChange={(e) => updateSearch(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Search;
