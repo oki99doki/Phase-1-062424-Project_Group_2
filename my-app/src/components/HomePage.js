e06062ff198be76d2fb7162209527d50663d4960
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-07-30 12:05:13 -0400

    Completed City card and rendering to the page and favorite button. Hi Rachel!

--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -11,8 +11,15 @@ const [searchPlaces, setSearchplaces] = useState("")
 
 useEffect(() => {
     fetch('http://localhost:4000/places')
-    .then(res => res.json())
+    .then(res => {
+      if(res.ok) {
+        return res.json()
+      } else {
+         throw Error ('Could not fetch the data from promise')
+      }
+    })
     .then(data => setDestination(data))
+    .catch(err => console.error('Was unable to reach the server for GET Request'))
   }, [])
 
 
@@ -20,7 +27,15 @@ useEffect(() => {
     setSearchplaces([...destinations, newPlaces])
   };
 
-
+  function updateFavorite(updatedDestination) {
+    setDestination(destinations.map(prevDestination => {
+      if (updatedDestination.id === prevDestination.id) {
+        return { ...prevDestination, favorite: updatedDestination.favorite };
+      } else {
+        return prevDestination;
+      }
+    }));
+  }
 
 
 return (
@@ -37,7 +52,7 @@ return (
   </div>
 
   {
-  destinations.map(destination => <CityList key={destination.id} destination={destination} />)
+  destinations.map(destination => <CityList key={destination.id} destination={destination} updateFavorite={updateFavorite} />)
   }
   
   </>
